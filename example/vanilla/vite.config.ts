89603c8d121160166f80baefa280203bea242814
Ismail Aslan <ismailaslan1097@gmail.com>
2025-05-26 22:21:57 +0200

    update vanilla example

---
+++ example/vanilla/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  root: __dirname, // This makes Vite treat this folder as the root
+  server: {
+    port: 5175,
+  },
+  resolve: {
+    alias: {
+      // Make sure your library import works in vanilla example
+      "typing-placeholder-input": resolve(
+        __dirname,
+        "../../dist/placeholder-typer.es.js"
+      ),
+    },
+  },
+});
